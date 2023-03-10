95005705d8efe4f2877d66819459fd332eb1d258
村上学 <>
2023-03-10 22:07:27 +0900

    4-42 コールバックの実装。４−４３トランザクション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 30 }
+    validate :validate_name_not_including_comma
+    before_validation :set_nameless_name
+  
+
+  private
+
+  def set_nameless_name
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&. include?(',')
+  end
 end
+
+  
